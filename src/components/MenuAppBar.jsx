6e779707738efd39be6d75bae6eb1f3e26363371
turao <lenz.arthur@gmail.com>
2019-03-18 23:26:10 -0300

    refactor: cleaning general components and home view to use pure functions

--- src/components/MenuAppBar.jsx
+++ src/components/MenuAppBar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
@@ -11,69 +11,49 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
 
-class MenuAppBar extends Component {
-  constructor(props) {
-    super(props);
+function MenuAppBar({ title }) {
+  const [logged, setLogged] = useState(false);
+  useEffect(() => setLogged(sessionStorage.getItem('logged')),
+    [sessionStorage.getItem('logged')]);
 
-    this.state = {
-      loggedIn: sessionStorage.getItem('logged'),
-    };
-  }
-
-
-  componentDidUpdate() {
-    this.hasLoggedInStatusChanged();
-  }
-
-  hasLoggedInStatusChanged() {
-    const { loggedIn } = this.state;
-    if (loggedIn !== Boolean(sessionStorage.getItem('logged'))) {
-      this.setState({ loggedIn: !loggedIn });
-    }
-  }
-
-  logout() {
+  const logout = () => {
     sessionStorage.removeItem('logged');
     sessionStorage.removeItem('userToken');
-    this.setState({ loggedIn: false });
-  }
+    setLogged(false);
+  };
 
-  render() {
-    const renderNotLoggedInContent = (
-      <div>
-        <Button component={Link} to="/login"> Log In </Button>
-        <Button component={Link} to="/signup"> Sign Up </Button>
-      </div>
-    );
+  const renderNotLoggedInContent = () => (
+    <div>
+      <Button component={Link} to="/login"> Log In </Button>
+      <Button component={Link} to="/signup"> Sign Up </Button>
+    </div>
+  );
 
-    const renderLoggedInContent = (
-      <React.Fragment>
-        <Button component={Link} to="/events"> Events </Button>
-        <Button component={Link} to="/account"> Me </Button>
-        <Button component={Link} onClick={this.logout} to="/"> Log Out </Button>
-      </React.Fragment>
-    );
+  const renderLoggedInContent = () => (
+    <React.Fragment>
+      <Button component={Link} to="/events"> Events </Button>
+      <Button component={Link} to="/account"> Me </Button>
+      <Button component={Link} onClick={logout} to="/"> Log Out </Button>
+    </React.Fragment>
+  );
 
-    const { title } = this.props;
-    const { loggedIn } = this.state;
-    return (
-      <React.Fragment>
-        <AppBar position="static">
-          <Toolbar>
+  return (
+    <React.Fragment>
+      <AppBar position="static">
+        <Toolbar>
 
-            <Typography variant="h6">
-              <Button component={Link} to="/">
-                { title }
-              </Button>
-            </Typography>
+          <Typography variant="h6">
+            <Button component={Link} to="/">
+              { title }
+            </Button>
+          </Typography>
 
-            { loggedIn ? renderLoggedInContent : renderNotLoggedInContent }
+          { logged ? renderLoggedInContent() : renderNotLoggedInContent() }
 
-          </Toolbar>
-        </AppBar>
-      </React.Fragment>
-    );
-  }
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
 }
 
 MenuAppBar.propTypes = {
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,27 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '@material-ui/core/Button';
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <Button component={Link} to="/events"> events </Button>
-        <Button component={Link} to="/factchecks"> factchecks </Button>
-        <Button component={Link} to="/organizations"> organizations </Button>
-        <Button component={Link} to="/parties"> parties </Button>
-        <Button component={Link} to="/statements"> statements </Button>
-        <Button component={Link} to="/users"> users </Button>
-      </React.Fragment>
-    );
-  }
+function Home() {
+  return (
+    <React.Fragment>
+      <Button component={Link} to="/events"> events </Button>
+      <Button component={Link} to="/factchecks"> factchecks </Button>
+      <Button component={Link} to="/organizations"> organizations </Button>
+      <Button component={Link} to="/parties"> parties </Button>
+      <Button component={Link} to="/statements"> statements </Button>
+      <Button component={Link} to="/users"> users </Button>
+    </React.Fragment>
+  );
 }
 
 export default Home;
